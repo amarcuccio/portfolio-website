885a28af7f67d7aa78788a35a8b740e56e160f1c
Andrew Marcuccio <marcuccio.andrew@gmail.com>
2023-01-31 23:15:36 -0500

    Updated menu titles, changed and optimized main page images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,14 +21,14 @@ export function Footer() {
           <Container.Inner>
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
+                <NavLink href="/">Home</NavLink>
                 <NavLink href="/about">About</NavLink>
+                <NavLink href="/blog">Blog</NavLink>
                 <NavLink href="/projects">Projects</NavLink>
-                <NavLink href="/speaking">Speaking</NavLink>
-                <NavLink href="/uses">Uses</NavLink>
+                <NavLink href="/contact">Contact</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
-                &copy; {new Date().getFullYear()} Andrew Marcuccio. All rights
-                reserved.
+                &copy; {new Date().getFullYear()} Andrew Marcuccio
               </p>
             </div>
           </Container.Inner>
--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -9,7 +9,7 @@ function Article({ article }) {
   return (
     <article className="md:grid md:grid-cols-4 md:items-baseline">
       <Card className="md:col-span-3">
-        <Card.Title href={`/articles/${article.slug}`}>
+        <Card.Title href={`/blog/${article.slug}`}>
           {article.title}
         </Card.Title>
         <Card.Eyebrow
@@ -38,7 +38,7 @@ export default function ArticlesIndex({ articles }) {
   return (
     <>
       <Head>
-        <title>Articles - Andrew Marcuccio</title>
+        <title>Blog - Andrew Marcuccio</title>
         <meta
           name="description"
           content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
