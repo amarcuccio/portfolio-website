ec5e32e758a5a28e3d44dd3ff868b018b55b29b0
Andrew Marcuccio <marcuccio.andrew@gmail.com>
2023-02-12 22:20:38 -0500

    Slight refactor of the contact form to prep for success message:

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,80 @@
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+
+export function ContactForm() {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm(
+      process.env.NEXT_PUBLIC_SERVICE,
+      process.env.NEXT_PUBLIC_TEMPLATE,
+      form.current,
+      process.env.NEXT_PUBLIC_USER
+    )
+    e.target.reset()
+  }
+
+  return (
+    <form ref={form} onSubmit={sendEmail} className="space-y-8">
+      <div>
+        <label
+          htmlFor="user_name"
+          className="mb-2 block text-base text-zinc-600 dark:text-zinc-400"
+        >
+          Your name
+        </label>
+        <input
+          type="text"
+          id="user_name"
+          name="user_name"
+          className="dark:shadow-sm-light block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 shadow-sm outline-none focus:border-teal-500 focus:ring-teal-500 dark:border-gray-600 dark:bg-zinc-800/90 dark:text-white dark:placeholder-gray-400 dark:focus:border-teal-500 dark:focus:ring-teal-500"
+          placeholder="Full name"
+          maxLength={70}
+          required
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="user_email"
+          className="mb-2 block text-base text-zinc-600 dark:text-zinc-400"
+        >
+          Your email
+        </label>
+        <input
+          type="email"
+          id="user_email"
+          name="user_email"
+          className="dark:shadow-sm-light block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm outline-none focus:border-teal-500 focus:ring-teal-500 dark:border-gray-600 dark:bg-zinc-800/90 dark:text-white dark:placeholder-gray-400 dark:focus:border-teal-500 dark:focus:ring-teal-500"
+          placeholder="email@domain.com"
+          maxLength={100}
+          required
+        />
+      </div>
+      <div className="sm:col-span-2">
+        <label
+          htmlFor="email_message"
+          className="mb-2 block text-base text-zinc-600 dark:text-zinc-400"
+        >
+          Your message
+        </label>
+        <textarea
+          id="email_message"
+          name="email_message"
+          rows="6"
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm outline-none focus:border-teal-500 focus:ring-teal-500 dark:border-gray-600 dark:bg-zinc-800/90 dark:text-white dark:placeholder-gray-400 dark:focus:border-teal-500 dark:focus:ring-teal-500"
+          placeholder="Leave a comment..."
+          maxLength={1000}
+          required
+        ></textarea>
+      </div>
+      <button
+        type="submit"
+        className="rounded-lg bg-teal-400 py-3 px-5 text-center text-base text-white hover:bg-teal-500 focus:outline-none focus:ring-2 focus:ring-teal-500 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-700 sm:w-fit"
+      >
+        Send message
+      </button>
+    </form>
+  )
+}
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,23 +1,8 @@
 import Head from 'next/head'
-import { useRef } from 'react'
-import emailjs from '@emailjs/browser'
+import { ContactForm } from '@/components/ContactForm'
 import { SimpleLayout } from '@/components/SimpleLayout'
 
 export default function Contact() {
-  const form = useRef()
-
-  const sendEmail = (e) => {
-    e.preventDefault()
-
-    emailjs.sendForm(
-      process.env.NEXT_PUBLIC_SERVICE,
-      process.env.NEXT_PUBLIC_TEMPLATE,
-      form.current,
-      process.env.NEXT_PUBLIC_USER
-    )
-    e.target.reset()
-  }
-
   return (
     <>
       <Head>
@@ -33,65 +18,7 @@ export default function Contact() {
         email using the form below. I look forward to hearing your thoughts, suggestions, and ideas."
       >
         <div className="max-w-screen-sm">
-          <form ref={form} onSubmit={sendEmail} className="space-y-8">
-            <div>
-              <label
-                htmlFor="user_name"
-                className="mb-2 block text-base text-zinc-600 dark:text-zinc-400"
-              >
-                Your name
-              </label>
-              <input
-                type="text"
-                id="user_name"
-                name="user_name"
-                className="dark:shadow-sm-light block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 shadow-sm outline-none focus:border-teal-500 focus:ring-teal-500 dark:border-gray-600 dark:bg-zinc-800/90 dark:text-white dark:placeholder-gray-400 dark:focus:border-teal-500 dark:focus:ring-teal-500"
-                placeholder="First and last name"
-                maxLength={70}
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="user_email"
-                className="mb-2 block text-base text-zinc-600 dark:text-zinc-400"
-              >
-                Your email
-              </label>
-              <input
-                type="email"
-                id="user_email"
-                name="user_email"
-                className="dark:shadow-sm-light block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm outline-none focus:border-teal-500 focus:ring-teal-500 dark:border-gray-600 dark:bg-zinc-800/90 dark:text-white dark:placeholder-gray-400 dark:focus:border-teal-500 dark:focus:ring-teal-500"
-                placeholder="email@domain.com"
-                maxLength={100}
-                required
-              />
-            </div>
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="email_message"
-                className="mb-2 block text-base text-zinc-600 dark:text-zinc-400"
-              >
-                Your message
-              </label>
-              <textarea
-                id="email_message"
-                name="email_message"
-                rows="6"
-                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm outline-none focus:border-teal-500 focus:ring-teal-500 dark:border-gray-600 dark:bg-zinc-800/90 dark:text-white dark:placeholder-gray-400 dark:focus:border-teal-500 dark:focus:ring-teal-500"
-                placeholder="Leave a comment..."
-                maxLength={1000}
-                required
-              ></textarea>
-            </div>
-            <button
-              type="submit"
-              className="rounded-lg bg-teal-400 py-3 px-5 text-center text-base text-white hover:bg-teal-500 focus:outline-none focus:ring-2 focus:ring-teal-500 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-700 sm:w-fit"
-            >
-              Send message
-            </button>
-          </form>
+          <ContactForm />
         </div>
       </SimpleLayout>
     </>
